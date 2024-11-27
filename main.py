e848482e69b4858989efbdde5c76830336bb9e6c
Evgenii44r <evgenii44r@gmail.com>
2024-11-27 23:04:55 +0300

    add

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ from tkinter import *
 from pickle import load, dump
 
 # область функций
-def set_status():
-    pass
+def save_game(event):
+    x1 = canvas.coords(player1)[0]
+    x2 = canvas.coords(player2)[0]
+    data = [x1, x2]
+    with open('save.dat', 'wb') as f:
+        dump(data, f)
+        set_status(text='Сохранено')
+
+
+
+def set_status(status_text,color = 'black'):
+    canvas.itemconfig(text_status_id, text=status_text, fill=color)
+
 
 def pause_toggle():
-    pass
+    global pause
+    pause = not pause
+    if pause:
+        canvas.itemconfig(text_status_id, text='Пауза', fill='black')
+    else:
+        canvas.itemconfig(text_status_id, text='Вперед', fill='black')
 
 
 def menu_toggle():
@@ -14,31 +30,58 @@ def menu_toggle():
 
 
 def key_handler(event):
-    pass
+    if game_over:
+        return
+    if event.keycode == KEY_PAUSE:
+        pause_toggle()
+
+    if pause:
+        return
+    set_status('Вперед')
+    if event.keycode == KEY_PLAYER1:
+        canvas.move(player1, SPEED, 0)
+    elif event.keycode == KEY_PLAYER2:
+        canvas.move(player2, SPEED, 0)
+    check_finish()
 
 def check_finish():
-    pass
-
+    global game_over
+    coords_player1 = canvas.coords(player1)
+    coords_player2 = canvas.coords(player2)
+    coords_finish = canvas.coords(finish_id)
+    x1_right = coords_player1[2]
+    x2_right = coords_player2[2]
+    x_finish = coords_finish[0]
+    if x1_right >= x_finish:
+        global status_text
+        set_status('победа верхнего игрока',player1_color)
+        game_over = True
+    if x2_right >= x_finish:
+        set_status('победа нижнего игрока', player2_color)
+        game_over = True
 
 def menu_enter():
     pass
 
-
 def game_new():
     pass
-
 
 def game_resume():
     pass
 
-
 def game_save():
     pass
 
-
-def game_load():
-    pass
-
+def game_load(event):
+    global x1, x2
+    with open('save.dat', 'rb') as f:
+        data = load(f)
+        x1, x2 = data
+        canvas.coords(player1, x1, y1, x1 + player_size, y1 + player_size)
+        canvas.coords(player2, x2, y2, x2 + player_size, y2 + player_size)
+        canvas.coords(x1, y1)
+        canvas.coords(x2, y2)
+        set_status(text ='Сохранено')
 
 def game_exit():
     pass
@@ -52,12 +95,16 @@ def menu_hide():
     pass
 
 
-def menu_up():
-    pass
-
-
 def menu_down():
     pass
+
+
+def menu_up():
+    global menu_current_index
+    menu_current_index -= 1
+    if menu_current_index < 0:
+        menu_current_index = 0
+    menu_update()
 
 
 def menu_update():
@@ -75,27 +122,22 @@ menu_mode = True
 menu_options = ['Возврат в игру', 'Новая игра', 'Сохранить', 'Загрузить', 'Выход']
 menu_current_index = 3
 menu_options_id = []
-
-
+x0 = 10
+offset = 100
 KEY_UP = 87
 KEY_DOWN = 83
 KEY_ESC = 27
 KEY_ENTER = 13
-
 player_size = 100
 x1, y1 = 50, 50
 x2, y2 = x1, y1 + player_size + 100
 player1_color = 'red'
 player2_color = 'blue'
-
 x_finish = game_width - 50
-
 KEY_PLAYER1 = 39
 KEY_PLAYER2 = 68
 KEY_PAUSE = 19
-
 SPEED = 12
-
 game_over = False
 pause = False
 
@@ -125,7 +167,7 @@ finish_id = canvas.create_rectangle(x_finish,
                                     game_height,
                                     fill='black')
 
-text_id = canvas.create_text(x1,
+text_status_id = canvas.create_text(x1,
                              game_height - 50,
                              anchor=SW,
                              font=('Arial', '25'),
@@ -134,4 +176,6 @@ text_id = canvas.create_text(x1,
 
 # Функции обратного вызова
 window.bind('<KeyRelease>', key_handler)
+window.bind('<Control-Key-s>',save_game)
+window.bind('<Control-Key-o>',game_width)
 window.mainloop()
